0a5093892577ce0652a7c8f04715110bfce1a79f
David Cole <david@davidcole.me>
2012-11-13 09:33:00 -0800

    Fix port for app

--- app.py
+++ app.py
@@ -54,4 +54,6 @@ def play():
     return render_template('board.jhtml', board=board)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
