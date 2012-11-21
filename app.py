333b89be5eefd202240ad6a53f989aad3fece932
David Cole <david@davidcole.me>
2012-11-20 23:27:45 -0800

    Postgres DB boilerplate

--- app.py
+++ app.py
@@ -1,37 +1,13 @@
-import sqlite3
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, request, g, redirect, url_for, render_template
 from contextlib import closing
 import os
 from random import randint
 
 
-DATABASE = 'tmp/rooklike.db'
-DEBUG = True
-SECRET_KEY = 'coprophage'
-USERNAME = 'admin'
-PASSWORD = 'rosetown'
-
-
 app = Flask(__name__)
-app.config.from_object(__name__)
-
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    g.db.close()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 
 def get_templates(filename):
