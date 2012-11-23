953f69fbe8e3c2a43f8214d1563e8c34ad737ea0
David Cole <david@davidcole.me>
2012-11-22 21:34:32 -0800

    Fix on event for mobile browsers

--- static/board.js
+++ static/board.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         debug(true);
     }
 
-    $(document).on('click',
+    $('tile').on('click',
         'piece.unlocked',
         function() {
             var self = $(this);
