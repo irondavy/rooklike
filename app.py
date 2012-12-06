e49fa40556472300a31b64d76ffe621627a190f7
David Cole <david@davidcole.me>
2012-12-06 14:54:45 -0800

    Add support for Twitter OAuth

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, session
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
+from flask.ext.login import LoginManager, current_user, login_required, login_user, logout_user
+from flask_oauth import OAuth
 import os
 from random import randint
 
@@ -11,14 +13,206 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres
 db = SQLAlchemy(app)
 
 
+app.secret_key = 'rj$96*0)hp_(@=4kgrtj39d@9-psqrd(_d%b!%*(#8@ds5ioao'
+
+
 class Board(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30))
+    bid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
+    uid = db.Column(db.Integer, db.ForeignKey('user.uid'))
     template = db.Column(db.String(1000))
 
-    def __init__(self, title, template):
+    def __init__(self, title, uid, template):
         self.title = title
+        self.uid = uid
         self.template = template
+
+
+class User(db.Model):
+    uid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    token = db.Column(db.String(50))
+    secret = db.Column(db.String(50))
+    boards = db.relationship('Board', backref='user', lazy='dynamic')
+
+    def __init__(self, name, token, secret):
+        self.name = name
+        self.token = token
+        self.secret = secret
+
+    def get_id(self):
+        return self.uid
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+
+login_manager = LoginManager()
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(uid):
+    return User.query.get(uid)
+
+login_manager.setup_app(app)
+
+
+oauth = OAuth()
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key='lZfre6Yks74x6M4RQWfxNA',
+    consumer_secret='mRltdabpGOsCciuaSSU5kqXg46f6roP1YsXbOFaM'
+)
+
+
+@twitter.tokengetter
+def get_twitter_token(token=None):
+    if current_user.is_authenticated():
+        return (current_user.token, current_user.secret)
+    else:
+        return None
+
+
+@app.route('/oauth-authorized')
+@twitter.authorized_handler
+def oauth_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        print('You denied the request to sign in.')
+        return redirect(next_url)
+
+    session['twitter_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+    session['twitter_user'] = resp['screen_name']
+
+    this_account = User.query.filter_by(name = resp['screen_name']).first()
+    if this_account is None:
+        new_account = User(resp['screen_name'], resp['oauth_token'], resp['oauth_token_secret'])
+        db.session.add(new_account)
+        db.session.commit()
+        login_user(new_account, remember=True)
+    else:
+        login_user(this_account, remember=True)
+
+    return redirect(next_url)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('boards'))
+
+
+@app.route('/boards')
+def boards():
+    boards = []
+    boards_query = Board.query.all()
+    boards_query.reverse()
+    for board in boards_query:
+        boards.append(dict(bid=board.bid, title=board.title, template=board.template))
+    return render_template('list.jhtml', boards=boards)
+
+
+@app.route('/login')
+def login():
+    if current_user.is_authenticated():
+        return redirect(url_for('index'))
+    return twitter.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route('/new')
+@login_required
+def new():
+    return render_template('new.jhtml', uid=current_user.uid)
+
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    uid = request.form['uid']
+    title = 'Untitled'
+    if request.form['title']:
+        title = request.form['title']
+    # Convert this to use .get()
+    template = request.form['template']
+    if validate_template(format_template(template)):
+        board = Board(title, uid, template)
+        db.session.add(board)
+        db.session.commit()
+        return redirect(url_for('boards'))
+    else:
+        return redirect(url_for('boards'))
+
+
+@app.route('/edit/<int:bid>')
+@login_required
+def edit(bid):
+    board_query = Board.query.get(bid)
+    title = board_query.title.upper()
+    template = board_query.template
+    viewer_uid = current_user.uid
+    board_uid = board_query.uid
+    if viewer_uid == board_uid:
+        return render_template('edit.jhtml', bid=bid, title=title, template=template)
+    else:
+        return redirect(url_for('play', bid=bid))
+
+
+@app.route('/delete', methods=['POST'])
+@login_required
+def delete():
+    bid = request.form['bid']
+    board = Board.query.get(bid)
+    db.session.delete(board)
+    db.session.commit()
+    return redirect(url_for('boards'))
+
+
+@app.route('/update', methods=['POST'])
+@login_required
+def update():
+    bid = request.form['bid']
+    template = request.form['template']
+    if validate_template(format_template(template)):
+        board = Board.query.get(bid)
+        board.title = request.form['title']
+        board.template = template
+        db.session.commit()
+    return redirect(url_for('play', bid=bid))
+
+
+@app.route('/play/<int:bid>')
+def play(bid):
+    board_query = Board.query.get(bid)
+    title = board_query.title.upper()
+    template = convert_template(format_template(board_query.template))
+    viewer_uid = current_user.uid
+    board_uid = board_query.uid
+    show_edit = True
+    if viewer_uid != board_uid:
+        show_edit = False
+    if template:
+        return render_template('board.jhtml', show_edit=show_edit, bid=bid, title=title, template=template)
+    else:
+        return redirect(url_for('boards'))
 
 
 def get_templates(filename):
@@ -127,76 +321,6 @@ def convert_template(template):
         y = y + 1
 
     return { 'squares': squares, 'width': x - 1, 'height': y - 1 }
-
-
-@app.route('/')
-def list():
-    boards = []
-    boards_query = Board.query.all()
-    boards_query.reverse()
-    for board in boards_query:
-        boards.append(dict(id=board.id, title=board.title, template=board.template))
-    return render_template('list.jhtml', boards=boards)
-
-
-@app.route('/new')
-def new():
-    return render_template('new.jhtml')
-
-
-@app.route('/add', methods=['POST'])
-def add():
-    title = 'Untitled'
-    if request.form['title']:
-        title = request.form['title']
-    template = request.form['template']
-    if validate_template(format_template(template)):
-        board = Board(title, template)
-        db.session.add(board)
-        db.session.commit()
-        return redirect(url_for('list'))
-    else:
-        return redirect(url_for('list'))
-
-
-@app.route('/edit/<int:id>')
-def edit(id):
-    board_query = Board.query.get(id)
-    title = board_query.title.upper()
-    template = board_query.template
-    return render_template('edit.jhtml', id=id, title=title, template=template)
-
-
-@app.route('/delete', methods=['POST'])
-def delete():
-    id = request.form['id']
-    board = Board.query.get(id)
-    db.session.delete(board)
-    db.session.commit()
-    return redirect(url_for('list'))
-
-
-@app.route('/update', methods=['POST'])
-def update():
-    id = request.form['id']
-    template = request.form['template']
-    if validate_template(format_template(template)):
-        board = Board.query.get(id)
-        board.title = request.form['title']
-        board.template = template
-        db.session.commit()
-    return redirect(url_for('play_id', id=id))
-
-
-@app.route('/play/<int:id>')
-def play_id(id):
-    board_query = Board.query.get(id)
-    title = board_query.title.upper()
-    board = convert_template(format_template(board_query.template))
-    if board:
-        return render_template('board.jhtml', id=id, title=title, board=board)
-    else:
-        return redirect(url_for('list'))
 
 
 if __name__ == '__main__':
